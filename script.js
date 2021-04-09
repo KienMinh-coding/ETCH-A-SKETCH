52bba2492f7933ecb9bf09519d2ddfdc0b5e9169
dongchijoseph <huyblogging@gmail.com>
2021-04-09 18:31:44 +0700

    add mouseover event

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ container.style.cssText = `grid-template-rows: repeat(${square}, auto); grid-tem
 for(let i = 0; i < square*square; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('square-style');
+    gridSquare.addEventListener('mouseover', () => {
+        gridSquare.classList.add('change-bgColor');
+    });
     container.appendChild(gridSquare);
 }
