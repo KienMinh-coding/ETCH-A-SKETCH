b7f91df1eb24275ae2cd2658616e9ab54906ff1e
dongchijoseph <huyblogging@gmail.com>
2021-04-09 17:44:01 +0700

    add square grid

---
+++ script.js
@@ -0,0 +1,11 @@
+let square = 16;
+
+const container = document.querySelector('#container');
+container.style.cssText = `grid-template-rows: repeat(${square}, auto); grid-template-columns: repeat(${square}, auto);`;
+
+
+for(let i = 0; i < square*square; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.classList.add('square-style');
+    container.appendChild(gridSquare);
+}
